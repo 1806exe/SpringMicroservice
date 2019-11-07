3cda98dc173f6ccac807a9d32e320f7d209a2587
Innocent Mgubhe <imgubhe@gmail.com>
2019-11-07 16:17:42 +0300

    Update logger function for both micro services

--- SpringNetflixAPIServer/src/main/java/com/SpringNetflixApiServerApplication.java
+++ SpringNetflixAPIServer/src/main/java/com/SpringNetflixApiServerApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
+@ComponentScan(basePackages = "com")
 @EnableZuulProxy
 @EnableDiscoveryClient
 @EnableAutoConfiguration
---
+++ SpringNetflixAPIServer/src/main/java/com/filter/ZulluLoggingFilter.java
@@ -0,0 +1,54 @@
+package com.filter;
+
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+import ch.qos.logback.classic.Level;
+
+@Component
+public class ZulluLoggingFilter extends ZuulFilter{
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	
+	@Override
+	public Object run() throws ZuulException{
+		System.out.println("Hello");
+		
+		HttpServletRequest request = RequestContext.getCurrentContext().getRequest();
+		logger.info("request ---> {} request URI -> {} " + request, request.getRequestURI());
+		logger.info("PRE filter called ", Level.INFO);
+		System.out.println("************************** request -->{} request URI-> {} " + request + " " + request.getRequestURI()+ "*****************************************");
+		return null;
+	}
+	
+	@Override
+	public boolean shouldFilter() {
+//		String header = RequestContext.getCurrentContext().getRequest()
+//				.getHeader("filter");
+//		if(header.equals("true")) {
+//			return true;
+//		}
+		
+		return true;
+	}
+	
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+	
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+	
+}
