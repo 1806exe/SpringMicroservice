9ca63f1227b1d4a1859f96f5c304f3518e433c1f
Innocent Mgubhe <imgubhe@gmail.com>
2019-11-07 13:16:42 +0300

    finish to integrate with zuul netflix server configuration

--- SpringCloudAPIService2/src/main/java/com/SpringCloudApiService2Application.java
+++ SpringCloudAPIService2/src/main/java/com/SpringCloudApiService2Application.java
@@ -2,10 +2,12 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com")
+@EnableDiscoveryClient
 public class SpringCloudApiService2Application {
 
 	public static void main(String[] args) {
--- SpringNetflixAPIServer/src/main/java/com/SpringNetflixApiServerApplication.java
+++ SpringNetflixAPIServer/src/main/java/com/SpringNetflixApiServerApplication.java
@@ -1,9 +1,15 @@
 package com;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableDiscoveryClient
+@EnableAutoConfiguration
 public class SpringNetflixApiServerApplication {
 
 	public static void main(String[] args) {
