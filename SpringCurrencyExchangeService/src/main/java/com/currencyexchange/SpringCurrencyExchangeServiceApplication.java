4910d55d8648f0e10e80c8d8da4c05fddbcd7273
Innocent Mgubhe <imgubhe@gmail.com>
2019-11-09 11:23:51 +0300

    add netflix hystrix module

--- SpringCurrencyExchangeService/src/main/java/com/currencyexchange/SpringCurrencyExchangeServiceApplication.java
+++ SpringCurrencyExchangeService/src/main/java/com/currencyexchange/SpringCurrencyExchangeServiceApplication.java
@@ -3,6 +3,7 @@ package com.currencyexchange;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -13,6 +14,7 @@ import brave.sampler.Sampler;
 @EntityScan(basePackages = "com.*")
 @EnableJpaRepositories(basePackages = "com.*")
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class SpringCurrencyExchangeServiceApplication {
 
 	public static void main(String[] args) {
--- SpringCurrencyExchangeService/src/main/java/com/currencyexchange/controllers/CurrencyExchangeController.java
+++ SpringCurrencyExchangeService/src/main/java/com/currencyexchange/controllers/CurrencyExchangeController.java
@@ -14,6 +14,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.currencyexchange.bean.ExchangeValue;
 import com.currencyexchange.exceptions.NoValuePresentException;
 import com.currencyexchange.repository.CurrencyExchnageRepository;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @RestController
 public class CurrencyExchangeController {
@@ -27,11 +29,20 @@ public class CurrencyExchangeController {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
 	
 	@GetMapping("/currency-exchange/to/{to}/from/{from}")
+	@HystrixCommand(commandProperties = {
+			@HystrixProperty(
+					name = "execution.isolation.thread.timeoutInMilliseconds", value = "12000"
+					)
+	})
 	public ExchangeValue getExchangeValue(@PathVariable String to, @PathVariable String from
 		) {
 		
 		logger.info("path varibale in exchange service --> {} from:- " + from + "to" + to);
-	
+		try {
+			Thread.sleep(9000);
+		}catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		ExchangeValue exchangeValue = repository.findByFromAndTo(from, to).get();
 		exchangeValue.setPort(Integer.parseInt(env.getProperty("local.server.port")));
 		return exchangeValue;
