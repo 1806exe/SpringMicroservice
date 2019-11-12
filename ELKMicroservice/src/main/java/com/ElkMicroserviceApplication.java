54e026425b672d248968f53138a1a53a4b5f667a
1806exe <1806exe@gmail.com>
2019-11-12 14:15:40 +0300

    finish with kibana integration and web view

--- ELKMicroservice/src/main/java/com/ElkMicroserviceApplication.java
+++ ELKMicroservice/src/main/java/com/ElkMicroserviceApplication.java
@@ -2,8 +2,10 @@ package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com")
+@ComponentScan(basePackages = "com")
 public class ElkMicroserviceApplication {
 
 	public static void main(String[] args) {
--- ELKMicroservice/src/main/java/com/controllers/MyELKController.java
+++ ELKMicroservice/src/main/java/com/controllers/MyELKController.java
@@ -1,39 +1,92 @@
 package com.controllers;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.Map;
 
+import org.apache.log4j.Logger;
+import org.apache.log4j.Priority;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+
+
 @RestController
 public class MyELKController {
-	
+
 	private static final Logger LOG = Logger.getLogger(MyELKController.class.getName());
-	
+
 	@Autowired
 	RestTemplate restTemplate;
-	
-	
+
 	@Bean
 	RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
+
 	
-	@RequestMapping(value = "/elkdemo/{username}")
+	
+	@GetMapping(value = "/elkdemo/{username}")
 	public String showGreetings(@PathVariable String username) {
 		String response = "Hello User ! " + username;
-		LOG.log(Level.INFO, "/elkdemo - " + response);
+		LOG.log(Priority.INFO, response);
+		return response;
+	}
+
+	
+	
+	@RequestMapping(value = "/elkdemo/user/{username}")
+	public String calculate(@PathVariable String username) {
+		ParameterizedTypeReference<Map<String, String>> tpeRef = new ParameterizedTypeReference<Map<String, String>>() {
+		};
+		String response = restTemplate.exchange("http://localhost:7171/elkdemo/" + username, HttpMethod.GET, null,
+				new ParameterizedTypeReference<String>() {
+				}, tpeRef).getBody().toString();
+
+		LOG.log(Priority.INFO, response);
+
+		try {
+
+			String exception = restTemplate.exchange("http://localhost:7171/exception/", HttpMethod.GET, null,
+					new ParameterizedTypeReference<String>() {
+					}, tpeRef).getBody().toString();
+			LOG.log(Priority.INFO, exception);
+			response = response + " === " + exception;
+
+		} catch (Exception e) {
+
+		}
+
 		return response;
 	}
 	
 	
 	
-	
-	
+
+	@RequestMapping(value = "/exception")
+	public String exception() {
+		String rsp = "";
+		try {
+			int i = 1 / 0;
+		} catch (Exception e) {
+			e.printStackTrace();
+			LOG.info(e.getMessage());
+			StringWriter sw = new StringWriter();
+			PrintWriter pw = new PrintWriter(sw);
+			e.printStackTrace();
+			String sStackTrace = pw.toString(); // stack trace string
+			LOG.info("Exception As String :: - &gt; " + sStackTrace);
+			rsp = sStackTrace;
+		}
+		return rsp;
+
+	}
 
 }
